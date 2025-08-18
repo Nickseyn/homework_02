f8e9aa1610db5fc9c89b0929523ef34412cab741
Nickseyn <Taras77880@gmail.com>
2025-08-18 21:07:31 +0300

    add controlled sliders with shared state and custom styles

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,10 +4,25 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx={{
+                color: '#00CC22',
+                width: 150,
+                height: 4,
+                '& .MuiSlider-thumb': {
+                    width: 16,
+                    height: 16,
+                    backgroundColor: '#01CB22',
+                },
+                '& .MuiSlider-track': {
+                    height: 4,
+                },
+                '& .MuiSlider-rail': {
+                    color: '#8B8B8B',
+                    opacity: 1,
+                    height: 4,
+                },
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props}
         />
     )
 }
