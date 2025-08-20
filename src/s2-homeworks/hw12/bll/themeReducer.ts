e8808a33a72683f88cb354980c636e663d4cf031
Nickseyn <Taras77880@gmail.com>
2025-08-20 21:16:46 +0300

    Add themeReducer with types, get themeId from Redux, handle theme change, set props for SuperSelect

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,23 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type ThemeState = {
+    themeId: number
+}
+
+type ThemeAction = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export const themeReducer = (state: ThemeState = initState, action: ThemeAction): ThemeState => { // fix any
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): any => ({type: 'SET_THEME_ID', id}) // fix any
